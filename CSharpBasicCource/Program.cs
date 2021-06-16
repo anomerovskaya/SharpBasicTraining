8e949bcbc557f3b83079358b3fa0c66999f767fb
Anastasiia Nomerovskaia <amoskvichka@gmail.com>
2021-06-16 10:15:34 +0300

    first commit

---
+++ CSharpBasicCource/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasicCource
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestClass.HelloName("Nastya");
+        }
+    }
+}
---
+++ CSharpBasicCource/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasicCource
+{
+    public class TestClass
+    {
+        public static void HelloName(string name)
+        {
+            string value3 = "data from value3"; 
+
+            Console.WriteLine("Hello " + "'" + value3 + "'");
+            Console.WriteLine(Sum(2, 3));
+            Console.ReadLine();
+        }
+
+        public static int  Sum(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
